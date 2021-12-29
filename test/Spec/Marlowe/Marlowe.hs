dominika-iohk/marlowe-cardano

Use small untyped validator

marlowe/test/Spec/Marlowe/Marlowe.hs
@@ -294,7 +294,7 @@ validatorSize = do
     let vsize = SBS.length. SBS.toShort . LB.toStrict $ Serialise.serialise validator
     let validator1 = Scripts.validatorScript $ smallTypedValidator defaultMarloweParams
     let vsize1 = SBS.length. SBS.toShort . LB.toStrict $ Serialise.serialise validator1
-    let validator2 = smallUntypedValidator defaultMarloweParams
+    let validator2 = Scripts.validatorScript $ smallUntypedValidator defaultMarloweParams
     let vsize2 = SBS.length. SBS.toShort . LB.toStrict $ Serialise.serialise validator2
     assertBool ("StateMachine Validator is too large " <> show vsize) (vsize < 18000)
     assertBool ("smallTypedValidator is too large " <> show vsize1) (vsize1 < 15000)

input-output-hk/marlowe-cardano-minimal

test/Spec/Marlowe/Marlowe.hs
@@ -353,7 +353,7 @@ typedValidatorSize = do
 
 untypedValidatorSize :: IO ()
 untypedValidatorSize = do
-    let validator = smallUntypedValidator defaultMarloweParams
+    let validator = Scripts.validatorScript $ smallUntypedValidator defaultMarloweParams
     let vsize = SBS.length. SBS.toShort . LB.toStrict $ Serialise.serialise validator
     assertBool ("smallUntypedValidator is too large " <> show vsize) (vsize < 14500)
 